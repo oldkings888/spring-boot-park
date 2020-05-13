bb9256285b098cd21c69308bc9e468bfc6f44fac
wangxu <wang.xu1@tcs.com>
2020-05-13 23:04:13 +0800

    init project

---
+++ src/main/java/com/oldk/springbootpark/SpringBootParkApplication.java
@@ -0,0 +1,13 @@
+package com.oldk.springbootpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oldk/springbootpark/SpringBootParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldk.springbootpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
